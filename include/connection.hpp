5efa7179b46b6c1898a6be1e92a82161e2f7bf6f
Mihir Lad <mihirlad55@gmail.com>
2020-07-10 00:47:33 -0400

    Add structures and types to connection.hpp
    Add structures like Monitors, Client, Layout, etc for holding properties after
    parsing the JSON results.

--- include/connection.hpp
+++ include/connection.hpp
@@ -7,6 +7,59 @@
 #include "packet.hpp"
 
 namespace dwmipc {
+typedef unsigned long Window;
+struct Monitor;
+struct Client;
+
+struct Client {
+    std::string name;
+    float mina, maxa;
+    int x, y, w, h;
+    int oldx, oldy, oldw, oldh;
+    int basew, baseh, incw, inch, maxw, maxh, minw, minh;
+    int bw, oldbw;
+    unsigned int tags;
+    bool isfixed, isfloating, isurgent, neverfocus, oldstate, isfullscreen;
+    int monitor_num;
+    Window win;
+};
+
+struct Layout {
+    std::string symbol;
+    uintptr_t address;
+};
+
+struct TagState {
+    unsigned int selected;
+    unsigned int occupied;
+    unsigned urgent;
+};
+
+struct Monitor {
+    std::string layout_symbol;
+    std::string old_layout;
+    std::string current_layout;
+    float mfact;
+    int nmaster;
+    int num;
+    int bar_y;          // bar geometry
+    int mx, my, mw, mh; // screen size
+    int wx, wy, ww, wh; // window area
+    unsigned int tagset;
+    unsigned int old_tagset;
+    bool show_bar;
+    bool top_bar;
+    std::vector<Window> win_clients;
+    std::vector<Window> win_stack;
+    Window win_sel;
+    Window win_bar;
+};
+
+enum Event {
+    EVENT_TAG_CHANGE = 1,
+    EVENT_SELECTED_CLIENT_CHANGE = 2,
+    EVENT_LAYOUT_CHANGE = 4
+};
 
 class Connection {
   public:
