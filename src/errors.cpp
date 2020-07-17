2e5c547ccdd1122fc9e94dde1d91a034599cd07d
Mihir Lad <mihirlad55@gmail.com>
2020-07-16 20:05:12 -0400

    Add format_generic in errors.cpp
    This function is for formatting errors that simply take a string as an argument
    and don't require any special formatting.

--- src/errors.cpp
+++ src/errors.cpp
@@ -10,6 +10,12 @@
 #include "dwmipcpp/errors.hpp"
 
 namespace dwmipc {
+static std::string format_generic(const std::string &msg) {
+    std::stringstream out;
+    out << "[dwmipcpp] " << msg;
+    return out.str();
+}
+
 static std::string format_errno(const std::string &msg = std::string()) {
     std::stringstream out;
     out << "[dwmipcpp] errno " << errno << ": " << msg << "(" << strerror(errno)
@@ -31,11 +37,6 @@ static std::string format_reply(const int expected, const int got) {
     return out.str();
 }
 
-static std::string format_failure(const std::string &reason) {
-    std::stringstream out;
-    out << "[dwmipcpp] " << reason;
-    return out.str();
-}
 
 HeaderError::HeaderError(const size_t read_bytes, const size_t to_read)
     : IPCError(format_eof(read_bytes, to_read)) {}
@@ -46,18 +47,16 @@ HeaderError::HeaderError(const std::string &msg)
 EOFError::EOFError(const size_t read_bytes, const size_t to_read)
     : IPCError(format_eof(read_bytes, to_read)) {}
 
-NoMsgError::NoMsgError()
-    : IPCError("No messages available") {}
+NoMsgError::NoMsgError() : IPCError(format_generic("No messages available")) {}
 
 ReplyError::ReplyError(const int expected, const int got)
     : IPCError(format_reply(expected, got)) {}
 
 ResultFailureError::ResultFailureError(const std::string &reason)
-    : IPCError(format_failure(reason)) {}
+    : IPCError(format_generic(reason)) {}
 
 ErrnoError::ErrnoError() : IPCError(format_errno()) {}
 
-ErrnoError::ErrnoError(const std::string &msg)
-    : IPCError(format_errno(msg)) {}
+ErrnoError::ErrnoError(const std::string &msg) : IPCError(format_errno(msg)) {}
 
 } // namespace dwmipc
